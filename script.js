18bf87a6bc07c757c2a6fd15f4b283c0a39250e2
AngelPelet <angelpeleteiro2001@gmail.com>
2023-12-04 18:48:26 -0300

    att_05

--- script.js
+++ script.js
@@ -26,6 +26,24 @@ for (i = 0; i<accordion.length; i++){
 });
 }
 
+const accordion1 = document.getElementsByClassName('label1');
+const accordionoff1 = document.getElementsByClassName('contentBx1');
+
+for (i = 0; i<accordion1.length; i++){
+  accordion1[i].addEventListener('click', function(){
+  accordionoff1.item(0).classList.toggle('ativo1')
+});
+}
+
+const accordion2 = document.getElementsByClassName('label2');
+const accordionoff2 = document.getElementsByClassName('contentBx2');
+
+for (i = 0; i<accordion2.length; i++){
+  accordion2[i].addEventListener('click', function(){
+  accordionoff2.item(0).classList.toggle('ativo2')
+});
+}
+
 
 //-----------------------------------------------------------------------------
 
@@ -71,6 +89,62 @@ function showSlides1(n) {
   }
   slidess[si-1].style.display = "block";  
   dotss[si-1].className += " active";
+}
+
+
+let si1 = 1;
+showSlides2(si1);
+
+function plusSlides2(n) {
+  showSlides2(si1 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(si1 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slidess1 = document.getElementsByClassName("fotos1");
+  let dotss1 = document.getElementsByClassName("dot2");
+  if (n > slidess1.length) {si1 = 1}    
+  if (n < 1) {si1 = slidess1.length}
+  for (i = 0; i < slidess1.length; i++) {
+    slidess1[i].style.display = "none";  
+  }
+  for (i = 0; i < dotss1.length; i++) {
+    dotss1[i].className = dotss1[i].className.replace(" active1", "");
+  }
+  slidess1[si1-1].style.display = "block";  
+  dotss1[si1-1].className += " active1";
+}
+
+
+let si2 = 1;
+showSlides3(si2);
+
+function plusSlides3(n) {
+  showSlides3(si2 += n);
+}
+
+function currentSlide3(n) {
+  showSlides3(si2 = n);
+}
+
+function showSlides3(n) {
+  let i;
+  let slidess2 = document.getElementsByClassName("fotos2");
+  let dotss2 = document.getElementsByClassName("dot3");
+  if (n > slidess2.length) {si2 = 1}    
+  if (n < 1) {si2 = slidess2.length}
+  for (i = 0; i < slidess2.length; i++) {
+    slidess2[i].style.display = "none";  
+  }
+  for (i = 0; i < dotss2.length; i++) {
+    dotss2[i].className = dotss2[i].className.replace(" active2", "");
+  }
+  slidess2[si2-1].style.display = "block";  
+  dotss2[si2-1].className += " active2";
 }
 
 
